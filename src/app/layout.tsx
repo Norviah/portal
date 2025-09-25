8338a4d592fe17af54eb7dfef097d2a2a0a61b7d
norviah <21983700+Norviah@users.noreply.github.com>
2025-09-25 09:33:49 -0700

    change default theme to light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${font.className} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
         <TailwindIndicator />
